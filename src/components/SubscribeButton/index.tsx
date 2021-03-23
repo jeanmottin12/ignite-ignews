22bfba03f2c404e1b95bbf46bf8f5cc7af5a8235
Jean Mottin <jeanmottin12@gmail.com>
2021-03-22 22:00:39 -0300

    add subscription feature with stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscriveButtonProps {
@@ -5,12 +8,34 @@ interface SubscriveButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscriveButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
-      Subscriebe now
+      Subscribe now
     </button>
   )
 }
